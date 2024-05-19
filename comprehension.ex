a630356a4069624c6a9bbe55206c5029951f4fe8
erfan1995 <aliriza.erfan@gmail.com>
2024-05-19 15:00:31 -0600

    more comprehension techniques

--- comprehension.ex
+++ comprehension.ex
@@ -1,4 +1,6 @@
 defmodule Comprehension do
+  #import Ineger module to use is_even function
+  import Integer
 
   #Nested loop
   def print_starss(list) do
@@ -8,9 +10,9 @@ defmodule Comprehension do
   end
 
   #Map loop
-  def map_com() do
+  def map_com do
     map = %{"a" => "A", "b" => "B", "c" => "C"}
-    for {k, v} <- map, do: k
+    for {k, _v} <- map, do: k
   end
 
   #String loop
@@ -19,9 +21,27 @@ defmodule Comprehension do
   end
 
   #loop with pattern matching
-  def pattern_comp() do
+  def pattern_comp do
     keyword_list = [ok: "Hello", error: "Unknown", ok: "World"]
     for {:ok, value} <- keyword_list, do: value
+  end
+
+  #filter through loop
+  def filter_comp do
+    for x <- 1..10, is_even(x), do: x
+  end
+
+  #muliple filters through loop
+  def multiple_filter_comp do
+    for x <- 1..100, is_even(x), rem(x,3)=== 0, do: x
+  end
+
+  #using into to change one collectable to another
+  #ex: converting keyword list to map
+  def into_comp do
+    list = [one: 1, two: 2]
+    for {k,v} <- list, into: %{}, do: {k,v}
+
   end
 
 end
